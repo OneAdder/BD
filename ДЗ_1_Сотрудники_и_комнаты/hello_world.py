0fbfda3cc2f8e02df1713e4397e106a3d5264d38
OneAdder <mikivo@list.ru>
2018-10-16 12:41:57 +0300

    Готово

--- ДЗ_1_Сотрудники_и_комнаты/hello_world.py
+++ ДЗ_1_Сотрудники_и_комнаты/hello_world.py
@@ -10,21 +10,34 @@ def show_table():
     data = sqliter.show_all()
     return render_template('show.html', table = data)
 
-@app.route('/add_worker')
+@app.route('/add_worker', methods=['GET', 'POST'])
 def add_worker():
-    if 'name' in request.args:
+    if request.method == 'POST':
+        message = ''
+        check = True
+        if not request.form['name']:
+            check = False
+            message += 'Вы забыли имя<br>'
+        if not request.form['patronymic']:
+            check = False
+            message += 'Вы забыли отчество<br>'
+        if not request.form['surename']:
+            check = False
+            message += 'Вы забыли фамилию<br>'
+        if not check:
+            return render_template('add_worker.html', message = message)
+        '''
         argus = ['name', 'patronymic', 'surename']
         check = True
         for ar in argus:
-            if ar not in request.args:
-                check = False
-                flash('You forgot ' + ar)
+            if not request.form['ar']:
+                message = 'asdfgsdf'
         if not check:
-            return redirect(url_for('add_worker'))
-
-        if sqliter.add_worker(request.args) == 1:
-            return redirect(url_for('add_worker', message = 'No rooms available. Rent a bigger office'))
-        return redirect(url_for('main_page', message = 'Added successfully'))
+            return redirect('/')
+        '''
+        if sqliter.add_worker(request.form) == 1:
+            return redirect(url_for('main_page', message = 'Нет свободных комнат. Найдите офис побольше'))
+        return render_template('add_worker.html', message = 'Успешно добавлен')
     else:
         return render_template('add_worker.html')
 
@@ -34,12 +47,12 @@ app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'databaseshse@gmail.com'
+app.config['MAIL_USERNAME'] = 'databeseshse@gmail.com'
 app.config['MAIL_PASSWORD'] = 'absolutely_secret'
 app.config['MAIL_DEBUG'] = True
 mail = Mail(app)
 app.config.from_object(__name__)
-app.config['ADMINS'] = ['databaseshse@gmail.com']
+app.config['ADMINS'] = ['databeseshse@gmail.com']
 
 @app.route('/')
 def main_page():
@@ -47,17 +60,15 @@ def main_page():
     with open('.secret_config', 'r', encoding = 'UTF-8') as f:
         check = f.read()
     if check != 'afklhsdfd\n':
-        print(check)
-        return 'a'
         message = 'Вы начали проверять моё дз. Информация об этом отправлена мне на почту, а так же вам на почту eklyshinsky@hse.ru'
-        email = request.form['email']
-        sender = request.form['person']
         msg = Message(message, sender=app.config['ADMINS'][0], recipients = ['mikivo@list.ru'])
         msg.body = message
         mail.send(msg)
         with open('.secret_config', 'w', encoding = 'UTF-8') as f:
-            check = f.write('')
-    return render_template('main.html', message = message)
+            check = f.write('afklhsdfd\n')
+        return render_template('main.html', message = message)
+    else:
+        return render_template('main.html', message = '')
 
 @app.route('/coffee')
 def teapot():
